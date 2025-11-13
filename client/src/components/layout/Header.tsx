1f25520292c05cd5e4078260e643856a562068d0
mj-fe <mj.fe.developer@gmail.com>
2025-11-13 15:54:57 -0300

    feat(client): base theme

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
-import { FaBars, FaChevronRight, FaChevronLeft } from 'react-icons/fa6';
+import { useTheme } from '@/hooks';
+import { FaBars, FaChevronRight, FaChevronLeft, FaSun, FaMoon } from 'react-icons/fa6';
 
 type HeaderProps = {
     isDesktop: boolean;
@@ -13,6 +14,8 @@ export default function Header({
     onToggleCollapse,
     onOpenMobile,
 }: HeaderProps) {
+    const { theme, toggleTheme } = useTheme();
+
     return (
         <header className="header">
             <div className="header-left">
@@ -24,16 +27,22 @@ export default function Header({
                 <h1 className="header-title">Fight Club</h1>
             </div>
 
-            {isDesktop && (
-                <button
-                    className="icon-btn"
-                    onClick={onToggleCollapse}
-                    aria-pressed={isCollapsed}
-                    aria-label={isCollapsed ? 'Expandir sidebar' : 'Colapsar sidebar'}
-                >
-                    {isCollapsed ? <FaChevronRight /> : <FaChevronLeft />}
+            <div className="menu-actions">
+                {isDesktop && (
+                    <button
+                        className="icon-btn"
+                        onClick={onToggleCollapse}
+                        aria-pressed={isCollapsed}
+                        aria-label={isCollapsed ? 'Expandir sidebar' : 'Colapsar sidebar'}
+                    >
+                        {isCollapsed ? <FaChevronRight /> : <FaChevronLeft />}
+                    </button>
+                )}
+
+                <button className="icon-btn" onClick={toggleTheme}>
+                    {theme === 'dark' ? <FaSun /> : <FaMoon />}
                 </button>
-            )}
+            </div>
         </header>
     );
 }
